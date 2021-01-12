44cf6284f49c0e861921d542520b3ede6a949e12
Stephen Duenas <semduenas@gmail.com>
2021-01-12 17:37:59 +0800

    Added node-sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './sass/main.scss';
 import App from './components/App';
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
